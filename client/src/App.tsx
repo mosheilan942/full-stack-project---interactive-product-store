f948abc6a2486e55ba49f004a5b52bc5664ae2ec
chezkin <hynymn@gmail.com>
2023-11-10 15:12:18 +0200

    {add} MUI + theme file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,25 @@
+import { Box, Container, Typography } from '@mui/material';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      app
-    </>
-  )
+    <Container sx={
+      {
+        height: '100vh', display: 'flex', flexDirection: 'column',
+         justifyContent: 'space-between', paddingBottom: '3%',
+      }
+    }
+      fixed maxWidth="xl">
+      <Box sx={{ my: 3, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        
+        <Typography variant='h3'>
+          app
+        </Typography>
+        
+      </Box>
+    </Container>
+  );
 }
 
 export default App
