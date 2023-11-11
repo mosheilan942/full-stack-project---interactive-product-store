066b2db97c3c4719ceac99e557eed98d1eb4055a
chezkin <hynymn@gmail.com>
2023-11-11 19:40:30 +0200

    {Refactoring} Arrangement - header, main, footer, and its grid design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,26 +5,42 @@ import router from './router/Router';
 
 import { store } from './Redux/store';
 import { Provider } from 'react-redux';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
- 
+
 
   const styleContainer = {
-    height: '100vh', display: 'flex', flexDirection: 'column',
-    justifyContent: 'space-between', paddingBottom: '3%',
-  }
+    height: '98vh',
+    display: 'grid',
+    gridTemplateAreas:
+      "'header header header' \
+      'main main main' \
+      'footer footer footer'",
+    gridTemplateRows: '80px 1fr 50px',
+    gridTemplateColumns: '15% 1fr 15%',
+    gridGap: '4px',
+  };
   const styleMainBox = {
-    my: 3, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'
+    my: 1, display: 'flex', flexDirection: 'column',
+    justifyContent: 'center', alignItems: 'center',
   }
 
- 
+
 
   return (
     <Container sx={styleContainer} fixed maxWidth="xl">
       <Provider store={store}>
-        <Box sx={styleMainBox}>
-        <RouterProvider router={router} />
+        <Box sx={{ ...styleMainBox, border: "2px solid red", gridArea: 'header' }}>
+          <Header />
+        </Box>
+        <Box sx={{ ...styleMainBox, border: "2px solid red", gridArea: 'main' }}>
+          <RouterProvider router={router} />
+        </Box>
+        <Box sx={{ ...styleMainBox, border: "2px solid red", gridArea: 'footer' }}>
+          <Footer />
         </Box>
       </Provider>
     </Container>
