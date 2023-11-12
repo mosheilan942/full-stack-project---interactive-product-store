40e9776e99c9623e2b4d6bd6d06eb854f82a30b1
ARIHAAAAAAAAA <ari05050700@gmail.com>
2023-11-12 11:21:36 +0200

    types and data

---
+++ server/types/cellPhone.ts
@@ -0,0 +1,20 @@
+export default interface CellPhone {
+    name: string;
+    manufacturer: string;
+    model: string;
+    price: number
+    category: string
+    color: string;
+    isKosher: boolean; 
+    screenSize: number;
+    dimensions: {
+        height: number;
+        length: number;
+        width: number;
+        weight: number;
+    };
+    quantity: number;
+    description: string;
+    image: string; // You can use the 'string' type for the image URL
+    addresses: string[];
+}
