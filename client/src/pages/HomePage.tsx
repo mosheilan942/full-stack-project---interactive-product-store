35779ec50ffd55c61ede4d3cd7a7e5f9e65ce158
Daniel Shemesh <ds8200@gmail.com>
2023-11-13 14:00:19 +0200

    Added home page.

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,28 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import Project from '../types/ProjectType';
-
 import { Typography, Box, Button, IconButton } from '@mui/material';
 
-// import { Outlet } from "react-router-dom";
-
-
-type Props = {
-
-}
+type Props = {};
 
 const HomePage = (props: Props) => {
-   
+    const [data, setData] = useState<Project[]>([]);
 
-    return (<>
-        <Box sx={{ display: 'flex' }}>
-            Home Page
-           
-        </Box>
+    useEffect(() => {
+        fetch("http://localhost:3000")
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
 
-        {/* <Outlet/> */}
-    </>
+    return (
+        <>
+            <Box sx={{ display: 'flex' }}>
+                <Typography variant="h4">Products:</Typography>
+            </Box>
 
-    )
-}
+            {data.map((project: Project) => (
+                <Box key={project.id} sx={{ background: "silver", width: 150, height: 250 }}>
+                    <Typography variant="h6">Name: {project.name}</Typography>
+                    <Typography variant="h6">Category: {project.category}</Typography>
 
-export default HomePage
+                </Box>
+            ))}
+        </>
+    );
+};
+
+export default HomePage;
