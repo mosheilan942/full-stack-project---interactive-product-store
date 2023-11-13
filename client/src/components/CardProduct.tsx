956a5fd43a79c17dfdb4638feefe9d8bd9c92fbe
Daniel Shemesh <ds8200@gmail.com>
2023-11-13 20:26:02 +0200

    addid style to cards

--- client/src/components/CardProduct.tsx
+++ client/src/components/CardProduct.tsx
@@ -20,9 +20,9 @@ const CardProduct = (props: Props) => {
             flexDirection: 'column'
         }}>
             <img src={"https://images.pexels.com/photos/7988747/pexels-photo-7988747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"} alt={product.name} style={{ width: '100%', height: '30%' }} />
-            <Typography sx={{ background: 'red', marginTop: 3 }} variant="h6">Name: </Typography>
+            <Typography sx={{ background: '#d55d31', marginTop: 3 }} variant="h6">Name: </Typography>
             {product.name}
-            <Typography sx={{ background: 'red', marginTop: 3 }} variant="h6">Category: </Typography>
+            <Typography sx={{ background: '#d55d31', marginTop: 3 }} variant="h6">Category: </Typography>
             {product.category}
             <Box>
                 <Button sx={{ background: 'skyblue', margin: '30px 0px' }}>View product</Button>
