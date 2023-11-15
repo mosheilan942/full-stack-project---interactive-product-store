8ddb2f38fa81a02f5e113de26b6f4d6cbfcce167
Daniel Shemesh <ds8200@gmail.com>
2023-11-15 09:57:38 +0200

    update prod page

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -3,18 +3,21 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductByID } from '../api/productFuncApi';
 import { CellPhoneType, RefrigeratorType, WashingMachineType } from '../types/ProductTypes';
+import { ExemplePhone } from '../data/typedb';
+import { exemplePhone } from '../data/db';
 
 type ProductType = CellPhoneType | RefrigeratorType | WashingMachineType;
 
 
+
 const ProductPage = () => {
   const { id } = useParams<{ id: string }>();
-  const [product, setProduct] = useState<ProductType | null>(null);
+  const [product, setProduct] = useState<ExemplePhone | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const getData = await getProductByID(id!)
-      setProduct(getData[0]);
+      // const getData = await getProductByID(id!)
+      setProduct(exemplePhone);
     };
 
     fetchData();
@@ -34,17 +37,33 @@ const ProductPage = () => {
   console.log(product)
   return (
     <Box sx={{ display: 'flex' }}>
-      <img src={product.image} alt={product.name} style={{ width: "50%" }} />
-      <Box>
-        <Typography variant="h4">{product._id}</Typography>
-        <Typography variant="body1">{product.name}</Typography>
-        <Typography variant="body1">{product.categoryType}</Typography>
-        <Typography variant="body1">{product.addresses}</Typography>
-        <Typography variant="body1">{product.date}</Typography>
-        <Typography variant="body1">{product.price}</Typography>
-        <Typography variant="body1">{product.model}</Typography>
-        <Typography variant="body1">{product.color}</Typography>
-
+      <Box sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "50%"
+      }}>
+        <img src={product.image} alt={product.name} style={{ width: 500 }} />
+      </Box>
+      <Box sx={{ margin: 5 }}>
+        <Typography variant="h4">{product.name}</Typography>
+        <Typography variant="body1">Category: {product.categoryType}</Typography>
+        <Typography variant="body1">Manufacturer: {product.manufacturer}</Typography>
+        <Typography variant="body1">Model: {product.model}</Typography>
+        <Typography variant="body1">Addresses: {product.addresses}</Typography>
+        <Typography variant="body1">Date: {product.date}</Typography>
+        <Typography variant="body1">Price: {product.price}</Typography>
+        <Typography variant="body1">Color: {product.color}</Typography>
+        <Typography variant="body1">Quantity: {product.quantity}</Typography>
+        <Typography variant="body1">Description: {product.description}</Typography>
+        <Typography variant="body1">Category Details: </Typography>
+        <ul>
+          <li>Height: {product.categoryDetails.height}</li>
+          <li>Width: {product.categoryDetails.width}</li>
+          <li>Weight: {product.categoryDetails.weight}</li>
+          <li>Length: {product.categoryDetails.length}</li>
+          <li>Screen size: {product.categoryDetails.screenSize}</li>
+        </ul>
       </Box>
     </Box>
   );
