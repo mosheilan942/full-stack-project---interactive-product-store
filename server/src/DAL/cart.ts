f143800a962f0bbfcb01b906c9a44cac71275d69
moshe ilan <moisi6510@gmail.com>
2023-11-16 14:14:14 +0200

    add mongo cluster

--- server/src/DAL/cart.ts
+++ server/src/DAL/cart.ts
@@ -7,8 +7,8 @@ export const addProductToCartData = async (userId: string, productId: string, op
     const userInDb = await UserModel.findById({ _id: userId }).select('cart').exec();
     const productsInUser = await UserModel.findOne({ _id: userId, "cart.productId": productId }).select({
         "cart.$": 1
-      }).exec();
-   
+    }).exec();
+
 
     if (!userInDb) throw new Error("Error: The user was not created properly")
     if (!productInDb) throw new Error("Error: There is no such product in the system")
@@ -25,28 +25,27 @@ export const addProductToCartData = async (userId: string, productId: string, op
                     }
                 }
             }).exec()
-            console.log("The product did not exist, and successfully added");    
+        console.log("The product did not exist, and successfully added");
         if (data) return data
         throw new Error("Error: The product was not added to the cart correctly Try again")
     }
     else {
-        console.log("moshe?");
-        const product = await UserModel.findOneAndUpdate({"cart.productId": productId},
-        {
-        })
+        const product = await UserModel.findOneAndUpdate({ "cart.productId": productId },
+            {
+            })
         // const productInUser = await UserModel.findById({ _id: userId }).findByIdAndUpdate({"cart.productId": productId}, {"quantity": 1}).exec();
         // const productInUser = await UserModel.findOneAndUpdate({ "cart.productId": productId }, { $set: { "quantity": 1 }}).exec();
         const productInUser2 = await UserModel.findOne({ "cart.productId": productId }).exec();
         const array = productInUser2?.cart
         array?.forEach((product, index) => {
-           if (String(product.productId) === productId) {
-            let quantity = product.quantity
-            operation ? ++quantity : --quantity
-            product.quantity = quantity
-            if (quantity === 0) {
-                array.splice(index, 1)
+            if (String(product.productId) === productId) {
+                let quantity = product.quantity
+                operation ? ++quantity : --quantity
+                product.quantity = quantity
+                if (quantity === 0) {
+                    array.splice(index, 1)
+                }
             }
-           } 
         })
         productInUser2?.save()
     }
--- server/src/Schemes/conectMongoose.ts
+++ server/src/Schemes/conectMongoose.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // const dbUrl = `mongodb+srv://${process.env.MONGODB_NAME}:${process.env.MONGODB_PASSWORD}@cluster0.ikepj5m.mongodb.net/?retryWrites=true&w=majority`;
-const dbUrl = `mongodb://127.0.0.1:27017/fullstack`;
+const dbUrl = `mongodb+srv://chezkin:chezkiN1234@cluster0.ikepj5m.mongodb.net/fullstack?retryWrites=true&w=majority`;
 
 // אופציות החיבור
 // const connectOptions: mongoose.ConnectOptions = {
