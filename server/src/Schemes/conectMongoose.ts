d14f2232bb50ff52d19c09baca309fe930b4d4ba
moshe ilan <moisi6510@gmail.com>
2023-11-16 21:23:41 +0200

    add errors

--- server/src/Schemes/conectMongoose.ts
+++ server/src/Schemes/conectMongoose.ts
@@ -3,11 +3,6 @@ import mongoose from "mongoose";
 const dbUrl = `mongodb+srv://chezkin:chezkiN1234@cluster0.ikepj5m.mongodb.net/fullstack?retryWrites=true&w=majority`;
 // const dbUrl = `mongodb://127.0.0.1:27017/fullstack`;
 
-// אופציות החיבור
-// const connectOptions: mongoose.ConnectOptions = {
-//   // useNewUrlParser: true,
-//   // useUnifiedTopology: true
-// };
 
 // מתחברים לבסיס הנתונים
 export const connectDB = () => {
