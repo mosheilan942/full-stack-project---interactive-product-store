1367b34bd3c54371c1a238e6ff8856270a8faf2d
chezkin <hynymn@gmail.com>
2023-11-16 08:40:53 +0200

    top 5 complete

--- client/src/components/home/HomePage.tsx
+++ client/src/components/home/HomePage.tsx
@@ -3,6 +3,8 @@ import { Box, Typography } from '@mui/material';
 import CardCategory from './CardCategory';
 import { getAllCategories, getTopCategories } from '../../api/productFuncApi';
 import { v4 as uuidv4 } from 'uuid';
+import CardProduct from '../cardProduct/CardProduct';
+import { ProductType } from '../../types/ProductTypes';
 
 type Category = {
     _id: string;
@@ -14,12 +16,12 @@ type Category = {
 }
 
 const HomePage = () => {
-    const [categoriesData, setCategoriesData] = useState<Category[][]| null>(null);
+    const [categoriesData, setCategoriesData] = useState<[Category[],ProductType[]] | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const data: Category[][] = await getTopCategories();
+                const data:[Category[],ProductType[]]  = await getTopCategories();
                 // console.log(data);
                 setCategoriesData(data);
 
@@ -42,13 +44,27 @@ const HomePage = () => {
             }}>
                 <Typography variant='h3'>Top Five categories</Typography>
             </Box>
-            
-                {categoriesData && categoriesData[0].map((category) => {
-                    // console.log('fd',categoriesData);
-                    
-                    return <CardCategory key={uuidv4()} category={category} />
-                })}
-           
+
+            {categoriesData && categoriesData[0].map((category) => {
+                return <CardCategory key={uuidv4()} category={category} />
+            })}
+
+            <Box sx={{
+                width: 1150,
+                height: 100,
+                display: "flex",
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Typography variant='h3'>Top Five Product</Typography>
+            </Box>
+
+            {categoriesData && categoriesData[1].map((product) => {
+                return <CardProduct key={uuidv4()}  product={product} />
+            })}
+
+
+
         </>
     );
 }
