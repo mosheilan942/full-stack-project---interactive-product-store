3176160c40e9d036afcd497fed0dbfedea626ed7
chezkin <hynymn@gmail.com>
2023-11-16 11:36:35 +0200

    complete

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductByID } from '../api/productFuncApi';
@@ -26,7 +26,7 @@ const ProductPage = () => {
   const navigate = useNavigate();
 
   const handleClickToComparison = () => {
-      navigate(`/comparison`);
+    navigate(`/comparison`);
   };
 
 
@@ -46,7 +46,34 @@ const ProductPage = () => {
   }
   return (
 
-    <>
+
+
+    <Box sx={{ margin: 5 }}>
+      <Typography variant="h4">{product.name}</Typography>
+      <Typography variant="body1">Category: {product.categoryType} .</Typography>
+      <Typography variant="body1">Manufacturer: {product.manufacturer} .</Typography>
+      <Typography variant="body1">Model: {product.model} .</Typography>
+      <Typography variant="body1">Addresses: {product.addresses} .</Typography>
+      <Typography variant="body1">Rating: {product.rating} .</Typography>
+      <Typography variant="body1">Date: {product.date} .</Typography>
+      <Typography variant="body1">Price: {product.price} .</Typography>
+      <Typography variant="body1">Color: {product.color} .</Typography>
+      <Typography variant="body1">Quantity: {product.quantity} .</Typography>
+      <Typography variant="body1">Description: {product.description} .</Typography>
+      {product.dimensions &&
+        <>
+          <br />
+          <Typography variant="body1">Dimensions:</Typography>
+          <ul>
+            {Object.entries(product.dimensions).map(([key, value]) => (
+              <li key={uuidv4()}>
+                {key.charAt(0).toUpperCase() + key.slice(1)}: {value}
+              </li>
+            ))}
+          </ul>
+        </>
+      }
+
       <Box sx={{ display: 'flex' }}>
         <Box sx={{
           display: 'flex',
@@ -54,65 +81,29 @@ const ProductPage = () => {
           alignItems: 'center',
           width: "50%"
         }}>
-          <Box >
+          <Grid sx={{ display: 'flex', justifyContent: 'space-around', margin: '4px', flexDirection: 'column', alignItems: 'center' }}>
             <img src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/iphone/iphone-14-pro-max-colors.png" alt={product.name} style={{ width: '50%' }} />
-          </Box>
+            <Button variant='contained' sx={{ margin: '4px', }}
+              onClick={() => dispatch(incrementToComparison(product))}
+            >
+              Add to Comparison
+            </Button >
+            <Button
+              onClick={handleClickToComparison}
+              variant='contained' sx={{ margin: '4px', }}>
+              Go to Comparison
+            </Button>
+            <AddToCartButtons product={product} />
+
+          </Grid>
           <Box sx={{ width: "350px", height: "350px" }}>
 
             <BasicMap />
           </Box>
         </Box>
-        <Box sx={{ margin: 5 }}>
-          <Typography variant="h4">{product.name}</Typography>
-          <Typography variant="body1">Category: {product.categoryType} .</Typography>
-          <Typography variant="body1">Manufacturer: {product.manufacturer} .</Typography>
-          <Typography variant="body1">Model: {product.model} .</Typography>
-          <Typography variant="body1">Addresses: {product.addresses} .</Typography>
-          <Typography variant="body1">Rating: {product.rating} .</Typography>
-          <Typography variant="body1">Date: {product.date} .</Typography>
-          <Typography variant="body1">Price: {product.price} .</Typography>
-          <Typography variant="body1">Color: {product.color} .</Typography>
-          <Typography variant="body1">Quantity: {product.quantity} .</Typography>
-          <Typography variant="body1">Description: {product.description} .</Typography>
-          {product.dimensions &&
-            <>
-              <br />
-              <Typography variant="body1">Dimensions:</Typography>
-              <ul>
-                {Object.entries(product.dimensions).map(([key, value]) => (
-                  <li key={uuidv4()}>
-                    {key.charAt(0).toUpperCase() + key.slice(1)}: {value}
-                  </li>
-                ))}
-              </ul>
-            </>
-          }
-
-    <Box sx={{ display: 'flex' }}>
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: "50%"
-      }}>
-        <Box sx={{display:'flex', justifyContent:'space-around',margin:'4px', flexDirection:'column', alignItems:'center'}}>
-        <img src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/iphone/iphone-14-pro-max-colors.png" alt={product.name} style={{ width: '50%' }} />
-        <Button variant='contained' sx={{margin:'4px', }}
-        onClick={() => dispatch(incrementToComparison(product))}
-        >
-          Add to Comparison
-        </Button >
-        <Button 
-        onClick={handleClickToComparison}
-        variant='contained' sx={{margin:'4px', }}>
-          Go to Comparison
-        </Button>
-        <AddToCartButtons product={product}/>
-
-        </Box>
       </Box>
-
-    </>
+    </Box>
   );
-};
-export default ProductPage;
+      }
+
+  export default ProductPage;
